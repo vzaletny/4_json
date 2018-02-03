d844eb0cf77a3ea15ffc5ef019ed7c976318a85e
vzaletny <vzaletny@gmail.com>
2018-02-04 00:59:24 +0700

    Initial commit
    Write minimal code and check functionality

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # Prettify JSON
 
-[TODO. There will be project description]
+This application gets any raw JSON file and prints it on the screen in convenient format
 
 # Quickstart
-
-[TODO]
 
 Example of script launch on Linux, Python 3.5:
 
 ```bash
+$ python pprint_json.py -f <path to JSON file>
+```
+Launch on Windows do the same way
 
-$ python pprint_json.py <path to file>
-# TODO add output example
+For instance:
 
+```bash
+python pprint_json.py c:\Temp\alco_shops.json
+```
+
+
+
+You can use -h option to get some help
+
+```python
+python pprint_json.py -h
+usage: pprint_json.py [-h] -f FILE
+
+Pretty print any raw JSON file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE, --file FILE  Incoming raw JSON file
 ```
 
 # Project Goals
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
 import json
 
 
+def get_args():
+    """
+    Supports the command-line arguments listed below.
+    """
+    parser = argparse.ArgumentParser(description='Pretty print any raw JSON file')
+    parser.add_argument('file', metavar='JSON file', help='Path to incoming raw JSON file')
+    args = parser.parse_args()
+    return args
+
+
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, 'r', encoding='utf-8') as open_file:
+            return json.load(open_file)
+    except FileNotFoundError:
+        pass
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
+
+
+def _main():
+    args = get_args()
+    json_file = load_data(args.file)
+    if json_file:
+        pretty_print_json(json_file)
+    else:
+        print('File not found')
 
 
 if __name__ == '__main__':
-    pass
+    _main()
