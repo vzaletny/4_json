12929afbd12e85f5490372ff868f652d60cfb822
vzaletny <vzaletny@gmail.com>
2018-02-04 15:51:04 +0700

    Fix all mentor's recommendation
    Remove docstring, shebang, UTF-8 hardcoding.
    Rename stupid variables' name.
    Reduce lines length.

--- pprint_json.py
+++ pprint_json.py
@@ -1,39 +1,40 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import argparse
 import json
 
 
 def get_args():
-    """
-    Supports the command-line arguments listed below.
-    """
-    parser = argparse.ArgumentParser(description='Pretty print any raw JSON file')
-    parser.add_argument('file', metavar='JSON file', help='Path to incoming raw JSON file')
+    parser = argparse.ArgumentParser(description='''Pretty print 
+                                                 any raw JSON file''')
+    parser.add_argument('file',
+                        metavar='JSON file',
+                        help='Path to incoming raw JSON file')
     args = parser.parse_args()
     return args
 
 
 def load_file(filepath):
-    try:
-        with open(filepath, 'r', encoding='utf-8') as open_file:
-            return json.load(open_file)
-    except FileNotFoundError:
-        pass
+    with open(filepath, 'r', encoding='utf-8') as open_file:
+        return json.load(open_file)
 
 
-def pretty_print_json(json_file):
-    print(json.dumps(json_file, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(json_content):
+    print(json.dumps(json_content,
+                     sort_keys=True,
+                     indent=4,
+                     ensure_ascii=False))
 
 
 def _main():
     args = get_args()
-    json_file = load_file(args.file)
-    if json_file:
-        pretty_print_json(json_file)
-    else:
+    try:
+        json_content = load_file(args.file)
+    except FileNotFoundError:
         print('File not found')
+    except OSError as error_msg:
+        print('Something went wrong')
+        print(error_msg)
+    else:
+        pretty_print_json(json_content)
 
 
 if __name__ == '__main__':
